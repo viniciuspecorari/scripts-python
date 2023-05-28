5f032c96beeb824e9c574c434079ebadb3117c83
Vinicius Pecorari <ovinicius.pecorari@gmail.com>
2023-05-28 14:18:02 -0300

    Add files via upload

---
+++ Scripts Aleatorios/text-to-speak.py
@@ -0,0 +1,12 @@
+from gtts import gTTS
+# from playsound import playsound
+
+audio = 'audio.mp3'
+language = 'pt-br'
+
+sp = gTTS(
+    text='Meu primeiro áudio gerado com Python',
+    lang=language
+)
+
+sp.save(audio)
